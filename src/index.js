be20fb28fc43c794ae2a4ccefd1928ec0a895687
ssmith-wombatweb <ssmith@wombatweb.us>
2018-07-10 21:37:46 -0700

    Validates local for standard characters. Need to add support for quoted special chars and comments.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 // VALID EMAIL
 // Checks if the email is valid.
 const validEmail = (email) => {
+  // Need to add support for special local chars that are properly quoted.
+  // Need to add support for quotes in ().
+  const localChars = 'abcdefghijklmnopqrstuvwxyz0123456789.'.split('');
+  const specialLocalChars = "!#$%&'*+-/=?^_`{|}~".split('');
+  const localLetters = email.slice(0, email.indexOf('@')).split('');
+  const areLocalCharsValid = () => localLetters.every(letter => localChars.indexOf(letter) !== -1);
+  if (!areLocalCharsValid()) return false;
+
   // Confirm that there is an at symbol and it isn't the first character.
   const atSymbol = email.indexOf('@');
   if (atSymbol < 1) return false;
@@ -17,8 +25,8 @@ const validEmail = (email) => {
   // Confirm that the domain name is only alpha and periods
   const domainChars = 'abcdefghijklmnopqrstuvwxyz.-'.split('');
   const domainLetters = email.slice(email.indexOf('@') + 1).split('');
-  const isDomainAlpha = () => domainLetters.every(letter => domainChars.indexOf(letter) > -1);
-  if (!isDomainAlpha()) return false;
+  const areDomainCharsValid = () => domainLetters.every(letter => domainChars.indexOf(letter) !== -1);
+  if (!areDomainCharsValid()) return false;
 
   return true;
 };
